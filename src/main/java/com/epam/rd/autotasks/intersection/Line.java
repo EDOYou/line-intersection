e892bad418be4b35ef60bb9aec1ad7396a5238b5
Taghiyev <ktaghiyev01@gmail.com>
2022-12-07 02:32:14 +0100

    line-intersection

--- src/main/java/com/epam/rd/autotasks/intersection/Line.java
+++ src/main/java/com/epam/rd/autotasks/intersection/Line.java
@@ -1,13 +1,25 @@
+//********************************************************************************* Line Intersection ******************************************************************************
+
 package com.epam.rd.autotasks.intersection;
 
 public class Line {
+    private final int k;
+    private final int b;
+
 
     public Line(int k, int b) {
-
+        this.k = k;
+        this.b = b;
     }
 
     public Point intersection(Line other) {
-        throw new UnsupportedOperationException();
+        int denom = (this.k - other.k); // Checking if denominator is ZERO
+        if (denom != 0) {
+            int x = (other.b - this.b) / (this.k - other.k);
+            int y = this.k * x + this.b;
+            return new Point(x, y);
+        }
+        else return null; // If denominator is ZERO, return NULL
     }
 
 }
